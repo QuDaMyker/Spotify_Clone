0051983add751d5a69d75fb37c9a9b70c466d661
VN The Boss Baby <93357243+QuDaMyker@users.noreply.github.com>
2023-05-09 14:29:12 +0700

    update

---
+++ app/src/main/java/com/example/spotify_clone/Activity/Login_Activity.java
@@ -0,0 +1,16 @@
+package com.example.spotify_clone.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.spotify_clone.R;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
